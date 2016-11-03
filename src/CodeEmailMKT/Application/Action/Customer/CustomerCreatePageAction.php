b637587c167a159e67797dbd425d98d4f56a5708
Rogerio de Souza Antonio <rogeriodesouzaantonio@ig.com.br>
2016-11-03 16:16:16 -0200

    Erro template

---
+++ src/CodeEmailMKT/Application/Action/Customer/CustomerCreatePageAction.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace CodeEmailMKT\Application\Action\Customer;
+
+use CodeEmailMKT\Domain\Entity\Customer;
+//use Symfony\Component\Routing\RouterInterface;
+use Zend\Diactoros\Response\RedirectResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Expressive\Router\RouterInterface;
+use Zend\Expressive\Template;
+use CodeEmailMKT\Domain\Persistence\CustomerRepositoryInterface;
+use Zend\Expressive\Template\TemplateRendererInterface;
+use Zend\Form\Form;
+use Zend\View\HelperPluginManager;
+
+class CustomerCreatePageAction
+{
+    private $template;
+    /**
+     * @var CustomerRepositoryInterface
+     */
+    private $repository;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+
+    public function __construct(CustomerRepositoryInterface $repository,
+                                TemplateRendererInterface $template,
+                                RouterInterface $router)
+    {
+        $this->template = $template;
+        $this->repository = $repository;
+        $this->router = $router;
+    }
+
+    public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
+    {
+        $myform = new Form();
+        $myform->add([
+            'name' => 'name',
+            'type' => 'Text',
+            'options' => [
+                'label' => 'Name:'
+            ]
+        ]);
+        $myform->add([
+            'name' => 'email',
+            'type' => 'Text',
+            'options' => [
+                'label' => 'E-mail:'
+            ]
+        ]);
+
+        $myform->add([
+            'name' => 'submit',
+            'type' => 'Submit',
+            'attributes' => [
+                'value' => 'Submit',
+            ],
+            'options' => [
+                'label' => 'Submit'
+            ]
+        ]);
+
+        if($request->getMethod() == 'POST'){
+            $flash = $request->getAttribute('flash');
+            $data = $request->getParsedBody();
+            $entity = new Customer();
+            $entity
+                ->setName($data['name'])
+                ->setEmail($data['email']);
+            $this->repository->create($entity);
+            $flash->setMessage('success','Contato cadastrado com sucesso');
+            $uri = $this->router->generateUri('customer.list');
+            return new RedirectResponse($uri);
+        }
+        return new HtmlResponse($this->template->render("app::customer/create",[
+            'myForm' => $myform
+        ]));
+
+    }
+}
